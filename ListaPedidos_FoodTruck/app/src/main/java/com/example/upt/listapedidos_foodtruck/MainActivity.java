944dc5d6b4768392b0bbaf19407561454a4fb2d1
FiorellaSalamanca <fiorellaexo@gmail.com>
2018-09-11 17:54:21 -0500

    Version 1

---
+++ ListaPedidos_FoodTruck/app/src/main/java/com/example/upt/listapedidos_foodtruck/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.upt.listapedidos_foodtruck;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    String texto = "";
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        Intent intent = getIntent();
+
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if(Intent.ACTION_SEND.equals(action) && type != null) //verifica si es diferente de nulo y a accion enviada
+        {
+            if("text/plain".equals(type)) //VERIFICA SI ES UN TEXT PLANO
+            {
+                //manipulamos la cadena enviada
+                texto = texto + intent.getStringExtra(Intent.EXTRA_TEXT);
+                manipularText(intent);
+            }
+        }
+    }
+
+    private void manipularText(Intent intent)
+    {
+        if(texto != null)
+        {
+            mTextMessage.setText("Lista de Pedidos: " + "\n" + "\n" + " - " + texto);
+            //Snackbar.make(findViewById(R.id.toolbar),texto, Snackbar.LENGTH_LONG).show(); //encontrar una vista
+            //Toast.makeText(MainActivity.this,texto, Toast.LENGTH_LONG).show();
+        }
+    }
+}
